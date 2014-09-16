2b0bacda950e0b514306c295859782f5a238e5d8
PascalVD <pascal.vandooren@gmail.com>
2014-09-16 10:35:29 +0200

    InternetCHK

---
+++ InternetCHK/InternetCHK.js
@@ -0,0 +1,19 @@
+exports.action = function(data, callback, config, SARAH){
+var config = config.modules.InternetCHK;
+
+var url = 'http://www.google.fr/';
+  
+   console.log(url);
+  var request = require('request');
+  request({ 'uri' : url }, function (err, response, body){
+    
+  if (err || response.statusCode != 200) {
+    callback({'tts': "pas de connexion internet"});
+    return;
+  }else{
+callback({'tts': "La connexion fonctionne correctement "});  
+  }
+ });
+
+
+}
