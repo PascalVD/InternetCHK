f679c168ad657926cf96c67e0d8f9919e2f66b1b
PascalVD <pascal.vandooren@gmail.com>
2014-09-17 11:30:00 +0200

    Adding Cron

--- InternetCHK/InternetCHK.js
+++ InternetCHK/InternetCHK.js
@@ -1,8 +1,9 @@
 exports.action = function(data, callback, config, SARAH){
+
 var config = config.modules.InternetCHK;
 
 var url = 'http://www.google.fr/';
-  
+
    console.log(url);
   var request = require('request');
   request({ 'uri' : url }, function (err, response, body){
@@ -15,5 +16,22 @@ callback({'tts': "La connexion fonctionne correctement "});
   }
  });
 
-
 }
+
+exports.cron = function(callback, task, SARAH){
+
+var url = 'http://www.google.fr/';
+
+   console.log(url);
+ var request = require('request');
+  request({ 'uri' : url }, function (err, response, body){
+    
+  if (err || response.statusCode != 200) {
+    callback({'tts': "pas de connexion internet"});
+    return;
+  }else{
+callback({'tts': "La connexion fonctionne correctement "});  
+  }
+ });
+}
+
