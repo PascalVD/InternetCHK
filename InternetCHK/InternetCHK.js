36200efbe9cba8ab25ffd4aa813a95fb54762100
PascalVD <pascal.vandooren@gmail.com>
2014-09-18 08:49:05 +0200

    Update 1.1
    optimize cron and connexion check

--- InternetCHK/InternetCHK.js
+++ InternetCHK/InternetCHK.js
@@ -1,37 +1,87 @@
-exports.action = function(data, callback, config, SARAH){
-
-var config = config.modules.InternetCHK;
-
-var url = 'http://www.google.fr/';
-
-   console.log(url);
-  var request = require('request');
-  request({ 'uri' : url }, function (err, response, body){
-    
-  if (err || response.statusCode != 200) {
-    callback({'tts': "pas de connexion internet"});
-    return;
-  }else{
-callback({'tts': "La connexion fonctionne correctement "});  
-  }
+exports.action = function(data, callback, config, SARAH)
+{
+ 
+        var config = config.modules.InternetCHK;
+ 
+        var url = 'http://www.google.fr/';
+ 
+  var fs   = require('fs');
+	fs.exists('./plugins/InternetCHK/stat.txt"', function (exists) {
+	var Statt = fs.createWriteStream('./plugins/InternetCHK/stat.txt');
+	Statt.end();
+	});
+  
+  var etat;
+  etat = fs.readFileSync("./plugins/InternetCHK/stat.txt", "UTF-8");
+  console.log("Demande explicite contenu fichier stat.txt = "+etat);
+ 
+ 
+        console.log(url);
+        var request = require('request');
+  request({ 'uri' : url }, function (err, response, body)
+  {
+   
+        if (err || response.statusCode != 200)
+        {
+     
+      if (etat != "NOK")
+      {
+        fs.writeFileSync("./plugins/InternetCHK/stat.txt", "NOK", "UTF-8");
+                                return;
+      }
+      callback({'tts': "pas de connexion internet"});
+        }
+        else
+        {
+      if (etat != "OK")
+      {
+        fs.writeFileSync("./plugins/InternetCHK/stat.txt", "OK", "UTF-8");
+                                return;
+      }
+      callback({'tts': "La connexion fonctionne correctement "});
+        }
  });
-
+ 
 }
-
-exports.cron = function(callback, task, SARAH){
-
-var url = 'http://www.google.fr/';
-
-   console.log(url);
- var request = require('request');
-  request({ 'uri' : url }, function (err, response, body){
-    
-  if (err || response.statusCode != 200) {
-    callback({'tts': "pas de connexion internet"});
-    return;
-  }else{
-callback({'tts': "La connexion fonctionne correctement "});  
-  }
+ 
+exports.cron = function(callback, task, SARAH)
+{
+  var url = 'http://www.google.fr/';
+ 
+  var fs   = require('fs');
+	fs.exists('./plugins/InternetCHK/stat.txt"', function (exists) {
+		var Statt = fs.createWriteStream('./plugins/InternetCHK/stat.txt');
+		Statt.end();
+	});
+  var etat;
+  etat = fs.readFileSync("./plugins/InternetCHK/stat.txt", "UTF-8");
+  console.log("CRON contenu fichier etat.txt = "+etat);
+ 
+ 
+        console.log(url);
+        var request = require('request');
+  request({ 'uri' : url }, function (err, response, body)
+  {
+   
+        if (err || response.statusCode != 200)
+        {
+     
+      if (etat != "NOK")
+      {
+        fs.writeFileSync("./plugins/InternetCHK/stat.txt", "NOK", "UTF-8");
+                callback({'tts': "pas de connexion internet"});
+                                return;
+      }
+        }
+        else
+        {
+      if (etat != "OK")
+      {
+        fs.writeFileSync("./plugins/InternetCHK/stat.txt", "OK", "UTF-8");
+        callback({'tts': "La connexion fonctionne correctement "});
+                                return;
+      }  
+        }
  });
 }
 
